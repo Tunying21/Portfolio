24447a4569d07428156d6d00ca7bb8f297b21178
Ed Rascoe <edcagatan00@gmail.com>
2026-07-23 09:44:16 +0800

    PortfolioWeb

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+@import "./assets/portfolio.css";
+
+#app {
+  margin: 0;
+  padding: 0;
+}
 </style>
